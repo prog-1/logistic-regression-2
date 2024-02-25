66a5263542fc69ddc35e6c9dae491e825134a3cc
vmaksimkina <valerijamax@gmail.com>
2024-02-25 17:50:23 +0200

    added cubic and linear regressions, images of data

--- example/main.go
+++ example/main.go
@@ -19,11 +19,16 @@ type InputReader interface {
 }
 
 const (
-	inputFileName = "data/blobs.csv"
-
+	inputFileName = "data/two_circles.csv"
 	epochs        = 1e+5
-	learningRateW = 1e-3
-	learningRateB = 1e-1
+	learningRateW = 1e-9
+	learningRateB = 3e-1
+	//cubic: arcs/two_circles/circle/exams   blobs:
+	//lrW, lrB = 1e-9, 3e-1                   1e-7, 4e-1
+	//quadratic two_circles:
+	//lrW, lrW =1e-7, 4e-1
+	//linear:  arcs/blobs/circle/     two_circles:
+	//lrW, lrB = 1e-3, 2e-1            1e-8, 2e-1
 )
 
 var (
@@ -32,8 +37,7 @@ var (
 
 func main() {
 	const screenWidth, screenHeight = 640, 480
-	ebiten.SetWindowSize(screenWidth, screenWidth)
-
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	var (
 		inputs [][]float64
 		y      []float64
@@ -51,9 +55,9 @@ func main() {
 			maxY = inputs[i][1]
 		}
 	}
-
 	inputs, y = shuffle(inputs, y)
-	qInputs := quadratic(inputs)
+	pow := 3
+	pInputs := polynomial(inputs, pow)
 	pointPlt := dataPlot{
 		posShape:   draw.PlusGlyph{},
 		negShape:   draw.RingGlyph{},
@@ -61,11 +65,11 @@ func main() {
 		trueClr:    color.RGBA{G: 255, A: 255},
 		falseClr:   color.RGBA{R: 255, A: 255},
 	}
-	xTrain, xTest, yTrain, yTest := split(qInputs, y)
+	xTrain, xTest, yTrain, yTest := split(pInputs, y)
 	for i := range yTrain {
 		pointPlt.addTrain(xTrain[i][0], xTrain[i][1], yTrain[i])
 	}
-	w := make([]float64, 5)
+	w := make([]float64, len(pInputs[0]))
 	var b float64
 	for i := 0; i < epochs; i++ {
 		p := inference(xTrain, w, b)
@@ -77,17 +81,15 @@ func main() {
 	}
 	fmt.Println("Weight:", w, "Bias:", b)
 	fmt.Println("Accuracy:", accuracy(xTest, yTest, w, b))
-
 	prob := inference(xTest, w, b)
 	for i := range prob {
 		pointPlt.addTest(xTest[i][0], xTest[i][1], yTest[i], prob[i])
 	}
-
 	boundPlot := decBoundPlot{
 		rows: int(maxY + 1.5),
 		cols: int(maxX + 1.5),
 		f: func(c, r int) float64 {
-			return p([]float64{float64(c), float64(r), float64(c * c), float64(r * r), float64(c * r)}, w, b)
+			return p(pPolynomial([]float64{float64(c), float64(r)}, pow), w, b)
 		},
 	}
 	plotters := []plot.Plotter{
@@ -97,24 +99,36 @@ func main() {
 	for _, p := range pps {
 		plotters = append(plotters, p)
 	}
-	if err := ebiten.RunGame(&App{img: ebiten.NewImageFromImage(Plot(screenWidth, screenHeight, plotters...))}); err != nil {
+	legend := fmt.Sprintf("Accuracy: %.2f", accuracy(xTest, yTest, w, b))
+	if err := ebiten.RunGame(&App{img: ebiten.NewImageFromImage(Plot(screenWidth, screenHeight, legend, plotters...))}); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func quadratic(inputs [][]float64) (qInputs [][]float64) {
-	for _, x := range inputs {
-		x0 := x[0]
-		x1 := x[1]
-		qInputs = append(qInputs, []float64{x0, x1, x0 * x0, x1 * x1, x0 * x1})
+func polynomial(inputs [][]float64, pow int) (pInputs [][]float64) {
+	pInputs = make([][]float64, len(inputs))
+	for i := range inputs {
+		pInputs[i] = pPolynomial([]float64{inputs[i][0], inputs[i][1]}, pow)
 	}
-	return qInputs
+	return pInputs
+}
+
+func pPolynomial(x []float64, pow int) (px []float64) {
+	px = []float64{x[0], x[1]}
+	for i := 0; i <= pow; i++ {
+		for j := 0; j <= pow-i; j++ {
+			if (i == 0 && j == 0) || (i == 1 && j == 0) || (i == 0 && j == 1) {
+				continue
+			}
+			px = append(px, math.Pow(x[0], float64(i))*math.Pow(x[1], float64(j)))
+		}
+	}
+	return px
 }
 
 func sigmoid(z float64) float64 {
 	return 1 / (1 + math.Exp(-z))
 }
-
 func dot(a []float64, b []float64) float64 {
 	var res float64
 	if len(a) != len(b) {
@@ -125,11 +139,9 @@ func dot(a []float64, b []float64) float64 {
 	}
 	return res
 }
-
 func p(x []float64, w []float64, b float64) float64 {
 	return sigmoid(dot(w, x) + b)
 }
-
 func inference(inputs [][]float64, w []float64, b float64) []float64 {
 	res := make([]float64, len(inputs))
 	for j, x := range inputs {
@@ -138,7 +150,6 @@ func inference(inputs [][]float64, w []float64, b float64) []float64 {
 	}
 	return res
 }
-
 func dCost(inputs [][]float64, y, p []float64) (dw []float64, db float64) {
 	m := float64(len(inputs))
 	dw = make([]float64, len(inputs[0]))
@@ -150,7 +161,6 @@ func dCost(inputs [][]float64, y, p []float64) (dw []float64, db float64) {
 	}
 	return dw, db
 }
-
 func accuracy(inputs [][]float64, y []float64, w []float64, b float64) (acc float64) {
 	r := inference(inputs, w, b)
 	for i := range inputs {
@@ -160,7 +170,6 @@ func accuracy(inputs [][]float64, y []float64, w []float64, b float64) (acc floa
 	}
 	return acc / float64(len(y))
 }
-
 func split(inputs [][]float64, y []float64) (xTrain, xTest [][]float64, yTrain, yTest []float64) {
 	size := len(inputs) / 5
 	return inputs[size:], inputs[:size], y[size:], y[:size]
